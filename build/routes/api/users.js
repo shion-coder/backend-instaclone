7d06f9f58124ee539f8841fad22ecf5f514a6202
Shion <shion.coder@gmail.com>
2020-07-27 18:20:40 +0700

    Edit gitignore

--- build/routes/api/users.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.usersRouter = void 0;
-var express_1 = require("express");
-/* -------------------------------------------------------------------------- */
-exports.usersRouter = express_1.Router();
-//# sourceMappingURL=users.js.map
