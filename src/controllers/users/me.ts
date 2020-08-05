fa89ab20bb7d7639bc2dfc8b0e0a8a433f9cfd4f
Shion <shion.coder@gmail.com>
2020-08-05 20:45:01 +0700

    Update result in route users/me

--- src/controllers/users/me.ts
+++ src/controllers/users/me.ts
@@ -11,7 +11,7 @@ export const me = async (req: AuthRequestProps, res: Response): Promise<Response
    * Get user after verify authentication & return user with out password
    */
 
-  const user = await User.findById(req.user?.id).select('-password');
+  const user = await User.findById(req.user?.id).select('-password -__v');
 
   if (!user) {
     return res.status(404).send({ error: errorMessage.noUser });
