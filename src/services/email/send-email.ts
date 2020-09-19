5e1f5784a1e122ec8a96baaf6381dd44f77a55f3
Shion <shion.coder@gmail.com>
2020-09-19 15:21:33 +0700

    Check resend email error

--- src/services/email/send-email.ts
+++ src/services/email/send-email.ts
@@ -1,7 +1,6 @@
 import nodemailer from 'nodemailer';
 
 import { MAIL_USER, MAIL_PASS } from '@config';
-import { logger } from '@logger';
 import { errorMessage } from '@messages';
 
 /* -------------------------------------------------------------------------- */
@@ -25,21 +24,18 @@ export const sendEmail = async (to: string, content: Record<string, unknown>): P
     from: MAIL_USER,
     to,
   };
-  console.log(MAIL_USER);
-  console.log(MAIL_PASS);
 
   /**
    * Combining the content and contacts into a single object that can be passed to Nodemailer.
    */
 
   const email = Object.assign({}, content, contacts);
-  console.log(email);
 
   try {
-    const info = await transporter.sendMail(email);
+    await transporter.sendMail(email);
 
-    logger.info(`Message sent: ${info.messageId}`);
-    logger.info(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
+    // logger.info(`Message sent: ${info.messageId}`);
+    // logger.info(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
   } catch {
     throw new Error(errorMessage.sendingEmail);
   }
