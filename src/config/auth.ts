229a820980131a0ccf04fdad23375654f4e03333
Shion <shion.coder@gmail.com>
2020-09-21 17:54:08 +0700

    Fi google signin

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,4 +1,3 @@
-import { ENVIRONMENT } from '@types';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, FB_CLIENT_ID, FB_CLIENT_SECRET } from './env';
 
 /* -------------------------------------------------------------------------- */
@@ -10,7 +9,7 @@ import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, FB_CLIENT_ID, FB_CLIENT_SECRET 
 const providers = ['google', 'facebook'];
 
 const callbacks = providers.map((provider) => {
-  return process.env.NODE_ENV === ENVIRONMENT.PRODUCTION ? '' : `/api/auth/${provider}/callback`;
+  return `/api/auth/${provider}/callback`;
 });
 
 const [googleCallbackURL, facebookCallbackURL] = callbacks;
